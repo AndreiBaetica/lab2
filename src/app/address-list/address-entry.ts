d46248aa5d8a90a14a1d6712544b89473b77314a
AndreiBaetica <andrei.baetic@gmail.com>
2020-10-05 00:03:12 -0400

    fixed repo

--- src/app/address-list/address-entry.ts
+++ src/app/address-list/address-entry.ts
@@ -3,14 +3,22 @@ export class AddressEntry {
   public lastName: string;
   public phone?: string;
   public email?: string;
+  public address?: string;
+  public city?: string;
+  public country?: string;
+  public postcode?: string;
   public notes?: string;
 
 
-  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, notes?: string) {
+  constructor(firstName?: string, lastName?: string, phone?: string, email?: string, address?: string, city?: string, country?: string, postcode?: string, notes?: string) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.email = email;
+    this.address = address;
+    this.city = city;
+    this.country = country;
+    this.postcode = postcode;
     this.notes = notes;
   }
 }
